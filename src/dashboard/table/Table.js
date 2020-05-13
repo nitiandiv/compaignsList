0994b1ca45fa935269804b78fcb22f4e6a0e45ef
divyansh_vs <dsdivsri60@gmail.com>
2020-05-13 20:00:50 +0530

    second lib\\\

--- src/dashboard/table/Table.js
+++ src/dashboard/table/Table.js
@@ -3,6 +3,10 @@ import { Table, Thead, Tbody, Tr, Th, Td } from "react-super-responsive-table";
 import "react-super-responsive-table/dist/SuperResponsiveTableStyle.css";
 import "./tableStyle.scss";
 import tableHeaders from "../../static/tableHeaders";
+import price from "../../assets/img/Price.png";
+import stats from "../../assets/img/stats.png";
+import file from "../../assets/img/file.png";
+import calendar from "../../assets/img/calendar.png";
 
 export default function TableExample(props) {
   console.log(
@@ -24,8 +28,32 @@ export default function TableExample(props) {
           <Td>
             <CampDetail />
           </Td>
-          <Td>item 2</Td>
-          <Td>item 2</Td>
+          <Td>
+            <img src={price} alt="none" /> view price
+          </Td>
+          <Td>
+            <div style={{display:'flex'}}>
+              <div><img src={file} alt="none" /> csv</div>
+              <div><img src={stats} alt="none" /> Reports</div>
+              <div><img src={calendar} alt="none" /> Schedule Again</div>
+            </div>
+          </Td>
+        </Tr>
+        <Tr>
+          <Td>item 1</Td>
+          <Td>
+            <CampDetail />
+          </Td>
+          <Td>
+            <img src={price} alt="none" /> view price
+          </Td>
+          <Td>
+            <div style={{display:'flex'}}>
+              <div><img src={file} alt="none" /> csv</div>
+              <div><img src={stats} alt="none" /> Reports</div>
+              <div><img src={calendar} alt="none" /> Schedule Again</div>
+            </div>
+          </Td>
         </Tr>
       </Tbody>
     </Table>
